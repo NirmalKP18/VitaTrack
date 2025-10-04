dde6fef7bea5485494ca43918a95f40f335f41ed
NirmalKP18 <nirmaledu7@gmail.com>
2025-10-04 12:20:58 +0530

    Onboarding screen 2

--- app/src/main/java/com/example/vitatrack/Onboarding2.kt
+++ app/src/main/java/com/example/vitatrack/Onboarding2.kt
@@ -5,17 +5,17 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class Onboarding1 : AppCompatActivity() {
+class Onboarding2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding1)
+        setContentView(R.layout.activity_onboarding2)
 
         val nextButton: Button = findViewById(R.id.nextButton)
 
         nextButton.setOnClickListener {
-            val intent = Intent(this, Onboarding2::class.java)
+            val intent = Intent(this, Onboarding3::class.java)
             startActivity(intent)
-            finish() // optional: closes Onboarding1 so user can't go back
+            finish() // optional: closes this activity so user can’t go back
         }
     }
 }
