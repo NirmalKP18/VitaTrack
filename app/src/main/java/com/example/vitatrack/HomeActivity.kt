ee5b7c7b276bf0c513426e1410ac69430a1c6b54
NirmalKP18 <nirmaledu7@gmail.com>
2025-10-04 16:09:57 +0530

    Fragments added

--- app/src/main/java/com/example/vitatrack/HomeActivity.kt
+++ app/src/main/java/com/example/vitatrack/HomeActivity.kt
@@ -1,20 +1,39 @@
 package com.example.vitatrack
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        // default screen
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+            bottomNav.selectedItemId = R.id.nav_home
         }
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> loadFragment(HomeFragment())
+                R.id.nav_habit -> loadFragment(HabitFragment())
+                R.id.nav_hydration -> loadFragment(HydrationFragment())
+                R.id.nav_mood -> loadFragment(MoodFragment())
+                R.id.nav_profile -> loadFragment(ProfileFragment())
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/vitatrack/HydrationFragment.kt
@@ -0,0 +1,17 @@
+package com.example.vitatrack
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HydrationFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hydration, container, false)
+    }
+}
