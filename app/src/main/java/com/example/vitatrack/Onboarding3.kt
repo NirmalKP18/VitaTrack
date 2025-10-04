30f54e45fbcc996752baf067f195ede9140b5d9b
NirmalKP18 <nirmaledu7@gmail.com>
2025-10-04 12:53:52 +0530

    Onboarding screen 3

--- app/src/main/java/com/example/vitatrack/Onboarding3.kt
+++ app/src/main/java/com/example/vitatrack/Onboarding3.kt
@@ -1,20 +1,21 @@
 package com.example.vitatrack
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Onboarding3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding3)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val getStartedButton: Button = findViewById(R.id.getStartedButton)
+
+        getStartedButton.setOnClickListener {
+            val intent = Intent(this, Login::class.java) // Change to HomeActivity if needed
+            startActivity(intent)
+            finish()
         }
     }
 }
